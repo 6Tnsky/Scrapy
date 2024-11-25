cfde7310b330420bc241d2e1b1e798149395820a
6Tnsky <n.6tnsky@gmail.com>
2024-11-25 17:47:44 +0300

    End

---
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -0,0 +1,17 @@
+import scrapy
+
+
+class DivannewparsSpider(scrapy.Spider):
+    name = "divannewpars"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
+
+    def parse(self, response):
+        divans = response.css('div.WdR1o')
+        for divan in divans:
+            yield {
+                'name' : divan.css('div.lsooF span::text').get(),
+                'price': divan.css('div.pY3d2 span::text').get(),
+                'urls': divan.css('a').attrib['href']
+            }
+
